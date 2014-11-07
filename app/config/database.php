d9bfd3b779afd0aa13380738ee31d0c550b5db41
moakdesigns <moakdesigns@gmail.com>
2014-11-07 13:07:37 -0500

    Update DB config for localhost

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'laravel',
+			'username'  => 'root',
+			'password'  => 'snc2014',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
