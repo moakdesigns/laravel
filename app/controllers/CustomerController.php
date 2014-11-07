101c0e3a864cbaf762d03535164cc3eb349a735f
moakdesigns <moakdesigns@gmail.com>
2014-11-07 16:48:29 -0500

    Updated Customer Controller to add new Customer
    added comments of what each function does & added capability of adding
    new customer

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -1,19 +1,22 @@
 <?php
-//
+
 class CustomerController extends BaseController {
 
+	// Get customer by their id
 	public function getIndex() {
 		$id = Input::get('id');
 		return Customer::find($id);
 	}
-
+	// Return all customers list
 	public function getAll() {
 		return Customer::all();
 	}
-
+	// Add new customer
 	public function postIndex() {
+		// checks if any input are empty
 		if (Input::has('first_name', 'last_name', 'email')) {
-			$input = Input::all();
+			$input = Input::all(); // this avoids calling Input::get() over & over
+			
 		}
 	}
 }
