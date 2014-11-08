1f833a2b9e16bcc422e3c7dacf57379927d6bac0
moakdesigns <moakdesigns@gmail.com>
2014-11-07 21:00:29 -0500

    Completed Customer Controller

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -7,16 +7,37 @@ class CustomerController extends BaseController {
 		$id = Input::get('id');
 		return Customer::find($id);
 	}
+
 	// Return all customers list
 	public function getAll() {
 		return Customer::all();
 	}
+
 	// Add new customer
 	public function postIndex() {
-		// checks if any input are empty
-		if (Input::has('first_name', 'last_name', 'email')) {
+		if (Input::has('first_name', 'last_name', 'email')) { 	// check inputs
 			$input = Input::all(); // this avoids calling Input::get() over & over
-			
+			if ($input['first_name'] == '' || $input['last_name'] == '' || $input['email'] == '') { 	// checks for empty input fields
+				return Response::make('You need to fill all of the fields', 400);	// if found empty, return HTTP 400 Bad Request error
+			}
+			// Create new Customer
+			$customer = new Customer;
+			$customer->first_name = $input['first_name'];
+			$customer->last_name = $input['last_name'];
+			$customer->email = $input['email'];
+			$customer->save();
+
+			return $customer;
+		} else  {
+				return Response::make('You need to fill out all fields', 400);	// return 400 if not all inputs filled
 		}
+	}
+
+	// Search for & delete customer
+	public function deleteIndex() {
+		$id = Input::get('id');	// get customer by id
+		$customer = Customer::find('id');	// search for customer
+		$customer->delete(); 	// delete customer
+		return $id;
 	}
 }
