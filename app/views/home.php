5279d8cc4e6c55406e97fb428190189474c4cdb4
moakdesigns <moakdesigns@gmail.com>
2014-11-11 17:04:20 -0500

    Minor code styling clean up

--- app/views/home.php
+++ app/views/home.php
@@ -7,3 +7,5 @@
 <script src="http://code.angularjs.org/1.2.3/angular.js"></script>
 <script src="http://code.angularjs.org/1.2.3/angular-route.js"></script>
 <script src="./app.js"></script>
+
+</html>
