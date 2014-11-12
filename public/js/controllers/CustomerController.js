867f62b9d94a51e61f691bb366337b6f3b5b2077
moakdesigns <moakdesigns@gmail.com>
2014-11-12 17:01:58 -0500

    Created CustomerController.js

---
+++ public/js/controllers/CustomerController.js
@@ -0,0 +1,4 @@
+// define Customer Controller
+app.controller('CustomersController', function CustomersController($scope, Data) {  // second parameter is the constructor function for controller, $scope is link between DOM & controller, second one is the service from the factory
+  
+})
